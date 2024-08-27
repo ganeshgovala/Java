4ed7d12b78207a026d114d87940181fa8164665c
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-08-27 15:08:09 +0530

    Update constructorDemo.java

--- Week3/constructorDemo.java
+++ Week3/constructorDemo.java
@@ -1,16 +1,8 @@
 class MyProgram {
-    
-    int x;
-    int y;
-    MyProgram(int x, int y) {
-        this.x = x;
-        this.y = y;
+    MyProgram() {
+        System.out.println("This is a default Constructor");
     }
-    
     public static void main(String args[]) {
-        MyProgram obj = new MyProgram(5, 4);
-        System.out.println(obj.y);
-        MyProgram obj2 = new MyProgram(6, 10);
-        System.out.println(obj2.y);
+        MyProgram obj = new MyProgram();
     }
 }
