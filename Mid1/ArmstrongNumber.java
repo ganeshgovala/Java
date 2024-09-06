8301fa3f6633c8639b80f5c84081d35a0e9ad181
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-09-06 09:19:50 +0530

    Create ArmstrongNumber.java

---
+++ Mid1/ArmstrongNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+        int original = num;
+        int res = 0;
+        int rem = 0;
+        while(num != 0) {
+            rem = num % 10;
+            res += Math.pow(rem, 3);
+            num /= 10;
+        }
+        if(res == original) {
+            System.out.println("Arm Strong Number");
+        } else {
+            System.out.println("Not an Arm Strong Number");
+        }
+    }
+}
