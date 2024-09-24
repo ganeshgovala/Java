6e01336ce8d48d2227d235309a670bf5766a1b7d
G. Sri Ram Ganesh <161675647+ganeshgovala@users.noreply.github.com>
2024-09-24 14:59:12 +0530

    Update BinarySearch.java

--- Week2/BinarySearch.java
+++ Week2/BinarySearch.java
@@ -1,63 +1,29 @@
-// Binary Search
-
 import java.util.*;
-
-class BinarySearch 
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the Number of Elements : ");
-		int size = sc.nextInt();
-		int array[] = new int[size];
-		int temp;
-		
-		// Reading Elements
-		
-		System.out.println("Enter the values : ");
-		for(int i = 0; i < size; i++)
-		{
-			array[i] = sc.nextInt();
-		}
-		
-		// Sorting Elements
-		
-		for(int i = 0; i < size - 1; i++) {
-			for(int j = 0; j < size - 1; j++) {
-				if(array[j] > array[j+1]) {
-					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-				}
-			}
-		}
-		
-		// Implementing Searching Operation
-		
-		int low = 0;
-		int mid;
-		int high = size - 1;
-		System.out.print("Enter Key to Search : ");
-		int key = sc.nextInt();
-		int count = 0;
-		
-		while(low <= high)
-		{
-			mid = (low + high) / 2;
-			if(array[mid] == key) {
-				count++;
-				break;
-			} else if(array[mid] < key) {
-				low = mid + 1;
-			} else {
-				high = mid - 1;
-			}
-		}
-		
-		if(count == 1) {
-			System.out.println("Element Found");
-		} else {
-			System.out.println("Element Not Found");
-		}
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i = 0; i < size; i++) {
+            System.out.print("Enter number : ");
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print("Enter Element to search : ");
+        int key = sc.nextInt();
+        int low = 0, mid = 0;
+        int high = size - 1;
+        while(low <= high) {
+            mid = (low + high) / 2;
+            if(arr[mid] == key) {
+                System.out.print("Element Found");
+                break;
+            } else if(arr[mid] > key) {
+                high = mid - 1;
+            } else if(arr[mid] < key) {
+                low = mid + 1;
+            }
+        }
+    }
 }
